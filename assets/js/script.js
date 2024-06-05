eb7d959747cc47edc22100bde0216868b5e629ce
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-05 09:23:18 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@
 jQuery(document).ready(function($) {
     // Example script
     console.log('SmartMail Assistant scripts loaded.');
+
+    // Add more interactivity as needed
+    $('.smartmail-button').on('click', function() {
+        alert('Button clicked!');
+    });
 });
