905c688f126c1f990beb4fd8764d533c016f1118
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-28 10:22:58 +0200

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -11,6 +11,7 @@ if (!function_exists('smartmail_register_shortcodes')) {
         add_shortcode('sma_follow_up_reminders', 'smartmail_follow_up_reminders_shortcode');
         add_shortcode('sma_sentiment_analysis', 'smartmail_sentiment_analysis_shortcode');
         add_shortcode('sma_email_templates', 'smartmail_email_templates_shortcode');
+        add_shortcode('sma_forensic_analysis', 'smartmail_forensic_analysis_shortcode');
     }
 }
 add_action('init', 'smartmail_register_shortcodes');
@@ -45,5 +46,9 @@ function smartmail_sentiment_analysis_shortcode($atts, $content = null) {
 
 function smartmail_email_templates_shortcode($atts, $content = null) {
     return smartmail_email_templates();
+}
+
+function smartmail_forensic_analysis_shortcode($atts, $content = null) {
+    return smartmail_forensic_analysis($content);
 }
 ?>
