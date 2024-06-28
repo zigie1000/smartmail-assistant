2a08f3ad914980b3d155de32aae6bbed4de2f159
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-28 22:06:06 +0200

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -1,5 +1,19 @@
 <?php
-// Shortcodes for displaying content
+// Register shortcodes
+if (!function_exists('smartmail_register_shortcodes')) {
+    function smartmail_register_shortcodes() {
+        add_shortcode('sma_email_categorization', 'smartmail_email_categorization_shortcode');
+        add_shortcode('sma_priority_inbox', 'smartmail_priority_inbox_shortcode');
+        add_shortcode('sma_automated_responses', 'smartmail_automated_responses_shortcode');
+        add_shortcode('sma_email_summarization', 'smartmail_email_summarization_shortcode');
+        add_shortcode('sma_meeting_scheduler', 'smartmail_meeting_scheduler_shortcode');
+        add_shortcode('sma_follow_up_reminders', 'smartmail_follow_up_reminders_shortcode');
+        add_shortcode('sma_sentiment_analysis', 'smartmail_sentiment_analysis_shortcode');
+        add_shortcode('sma_email_templates', 'smartmail_email_templates_shortcode');
+        add_shortcode('sma_forensic_analysis', 'smartmail_forensic_analysis_shortcode');
+    }
+}
+add_action('init', 'smartmail_register_shortcodes');
 
 if (!function_exists('smartmail_email_categorization_shortcode')) {
     function smartmail_email_categorization_shortcode($atts, $content = null) {
@@ -25,8 +39,7 @@ if (!function_exists('smartmail_email_summarization_shortcode')) {
     }
 }
 
-if (!function_exists('smartmail_meeting_scheduler_shortcode'))
-{
+if (!function_exists('smartmail_meeting_scheduler_shortcode')) {
     function smartmail_meeting_scheduler_shortcode($atts, $content = null) {
         return smartmail_meeting_scheduler($content);
     }
@@ -55,20 +68,3 @@ if (!function_exists('smartmail_forensic_analysis_shortcode')) {
         return smartmail_forensic_analysis($content);
     }
 }
-
-// Register the shortcodes
-if (!function_exists('smartmail_register_shortcodes')) {
-    function smartmail_register_shortcodes() {
-        add_shortcode('sma_email_categorization', 'smartmail_email_categorization_shortcode');
-        add_shortcode('sma_priority_inbox', 'smartmail_priority_inbox_shortcode');
-        add_shortcode('sma_automated_responses', 'smartmail_automated_responses_shortcode');
-        add_shortcode('sma_email_summarization', 'smartmail_email_summarization_shortcode');
-        add_shortcode('sma_meeting_scheduler', 'smartmail_meeting_scheduler_shortcode');
-        add_shortcode('sma_follow_up_reminders', 'smartmail_follow_up_reminders_shortcode');
-        add_shortcode('sma_sentiment_analysis', 'smartmail_sentiment_analysis_shortcode');
-        add_shortcode('sma_email_templates', 'smartmail_email_templates_shortcode');
-        add_shortcode('sma_forensic_analysis', 'smartmail_forensic_analysis_shortcode');
-    }
-}
-add_action('init', 'smartmail_register_shortcodes');
-?>
