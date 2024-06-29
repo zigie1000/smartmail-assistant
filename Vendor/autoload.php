b74564d1c6897b9fc6a0a2286a17ce3515f630f2
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-06-29 14:14:13 +0200

    Update autoload.php

--- Vendor/autoload.php
+++ Vendor/autoload.php
@@ -1,25 +1,30 @@
 <?php
-spl_autoload_register(function ($class) {
-    // Define the base directory for the namespace prefix
-    $baseDir = __DIR__ . '/../includes/';
 
-    // Does the class use the namespace prefix?
-    $len = strlen('SmartMailAssistant\\');
-    if (strncmp('SmartMailAssistant\\', $class, $len) !== 0) {
-        // No, move to the next registered autoloader
+spl_autoload_register(function ($class) {
+    // project-specific namespace prefix
+    $prefix = 'SmartMailAssistant\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/../includes/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
         return;
     }
 
-    // Get the relative class name
-    $relativeClass = substr($class, $len);
+    // get the relative class name
+    $relative_class = substr($class, $len);
 
-    // Replace the namespace prefix with the base directory, replace namespace
+    // replace the namespace prefix with the base directory, replace namespace
     // separators with directory separators in the relative class name, append
     // with .php
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-    // If the file exists, require it
+    // if the file exists, require it
     if (file_exists($file)) {
         require $file;
     }
 });
+?>
