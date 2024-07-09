9b5af87795ad941a58d6a8af7ac5ce1c8bcd559b
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-07-09 11:22:32 +0200

    Update admin-settings.php

--- includes/admin-settings.php
+++ includes/admin-settings.php
@@ -4,56 +4,42 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-if (!function_exists('smartmail_register_settings')) {
-    function smartmail_register_settings() {
-        register_setting('smartmail-settings-group', 'smartmail_openai_api_key');
-        register_setting('smartmail-settings-group', 'smartmail_email_username');
-        register_setting('smartmail-settings-group', 'smartmail_email_password');
-    }
+function smartmail_register_settings() {
+    register_setting('smartmail-settings-group', 'smartmail_openai_api_key');
+    register_setting('smartmail-settings-group', 'smartmail_email_username');
+    register_setting('smartmail-settings-group', 'smartmail_email_password');
 }
 
-if (!function_exists('smartmail_settings_page')) {
-    function smartmail_settings_page() {
-        add_options_page('SmartMail Settings', 'SmartMail', 'manage_options', 'smartmail-settings', 'smartmail_render_settings_page');
-    }
+function smartmail_settings_page() {
+    add_options_page('SmartMail Settings', 'SmartMail', 'manage_options', 'smartmail-settings', 'smartmail_render_settings_page');
 }
 
-if (!function_exists('smartmail_render_settings_page')) {
-    function smartmail_render_settings_page() {
-        ?>
-        <div class="wrap">
-            <h1>SmartMail Settings</h1>
-            <form method="post" action="options.php">
-                <?php settings_fields('smartmail-settings-group'); ?>
-                <?php do_settings_sections('smartmail-settings-group'); ?>
-                <table class="form-table">
-                    <tr valign="top">
-                        <th scope="row">OpenAI API Key</th>
-                        <td><input type="text" name="smartmail_openai_api_key" value="<?php echo esc_attr(get_option('smartmail_openai_api_key')); ?>" /></td>
-                    </tr>
-                    <tr valign="top">
-                        <th scope="row">Email Username</th>
-                        <td><input type="text" name="smartmail_email_username" value="<?php echo esc_attr(get_option('smartmail_email_username')); ?>" /></td>
-                    </tr>
-                    <tr valign="top">
-                        <th scope="row">Email Password</th>
-                        <td><input type="password" name="smartmail_email_password" value="<?php echo esc_attr(get_option('smartmail_email_password')); ?>" /></td>
-                    </tr>
-                </table>
-                <?php submit_button(); ?>
-            </form>
-        </div>
-        <?php
-    }
+function smartmail_render_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>SmartMail Settings</h1>
+        <form method="post" action="options.php">
+            <?php settings_fields('smartmail-settings-group'); ?>
+            <?php do_settings_sections('smartmail-settings-group'); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">OpenAI API Key</th>
+                    <td><input type="text" name="smartmail_openai_api_key" value="<?php echo esc_attr(get_option('smartmail_openai_api_key')); ?>" /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Email Username</th>
+                    <td><input type="text" name="smartmail_email_username" value="<?php echo esc_attr(get_option('smartmail_email_username')); ?>" /></td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row">Email Password</th>
+                    <td><input type="password" name="smartmail_email_password" value="<?php echo esc_attr(get_option('smartmail_email_password')); ?>" /></td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
 }
-
-function smartmail_enqueue_admin_scripts($hook) {
-    if ($hook != 'settings_page_smartmail-settings') {
-        return;
-    }
-    wp_enqueue_script('smartmail_admin_script', plugin_dir_url(__FILE__) . '../assets/js/script.js', array('jquery'), '1.0.0', true);
-}
-add_action('admin_enqueue_scripts', 'smartmail_enqueue_admin_scripts');
 
 add_action('admin_init', 'smartmail_register_settings');
 add_action('admin_menu', 'smartmail_settings_page');
