7e36c4c24f586a04ea54f59c60f8d1294d9c383f
zigie1000 <37893376+zigie1000@users.noreply.github.com>
2024-07-12 15:54:43 +0200

    Update smartmail-dashboard.php

--- includes/templates/smartmail-dashboard.php
+++ includes/templates/smartmail-dashboard.php
@@ -2,6 +2,7 @@
 /*
 Template Name: SmartMail Dashboard
 */
+
 get_header(); ?>
 
 <div class="smartmail-dashboard">
@@ -84,51 +85,5 @@ get_header(); ?>
         </div>
     </div>
 </div>
-
-<style>
-.smartmail-dashboard {
-    display: flex;
-}
-
-.smartmail-sidebar {
-    width: 200px;
-    background-color: #f4f4f4;
-    padding: 20px;
-}
-
-.smartmail-sidebar h2 {
-    margin: 0;
-    padding: 0 0 20px 0;
-    border-bottom: 1px solid #ccc;
-}
-
-.smartmail-sidebar ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-.smartmail-sidebar ul li {
-    margin: 10px 0;
-}
-
-.smartmail-sidebar ul li a {
-    text-decoration: none;
-    color: #333;
-}
-
-.smartmail-content {
-    flex: 1;
-    padding: 20px;
-}
-
-.smartmail-section {
-    margin-bottom: 40px;
-}
-
-.smartmail-section h2 {
-    border-bottom: 2px solid #333;
-    padding-bottom: 10px;
-}
-</style>
 
 <?php get_footer(); ?>
